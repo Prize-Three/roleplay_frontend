dfad7ad022f91c96bc0b8a9266c354bc1f183e2c
SeoMiYoung <seo20seo00@naver.com>
2024-07-01 01:34:39 +0900

    기본 세팅

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import styles from './Home.module.scss';
+import { useNavigate } from 'react-router-dom';
+
+function Home() {
+    const navigate = useNavigate();
+    let title = 'AI와 함께하는 역할놀이 서비스';
+
+    return (
+        <div className={styles.homeDiv}>
+            <h4>{title}</h4>
+            <button onClick={() => navigate('/situation')}>시작하기</button>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/pages/Situation/Situation.jsx
@@ -0,0 +1,13 @@
+import styles from './Situation.module.scss'
+
+function Situation() {
+    return (
+        <div>
+            <button>병원놀이</button>
+            <button>시장놀이</button>
+            <button>가족놀이</button>
+        </div>
+    )
+}
+
+export default Situation;
