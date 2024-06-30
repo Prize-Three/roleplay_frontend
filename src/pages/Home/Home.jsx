a738811ebf0d64909a93f5908489d5546a7f1248
SeoMiYoung <seo20seo00@naver.com>
2024-07-01 02:19:22 +0900

    home page에 이미지 넣어보기

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,10 +6,15 @@ function Home() {
     let title = 'AI와 함께하는 역할놀이 서비스';
 
     return (
-        <div className={styles.homeDiv}>
-            <h4>{title}</h4>
-            <button onClick={() => navigate('/situation')}>시작하기</button>
-        </div>
+        <>
+            <div className={styles.homeImg}>
+                <img src="/assets/home/homeImg.png" width="100%"/>
+            </div>
+            <div className={styles.homeDiv}>
+                <h4>{title}</h4>
+                <button onClick={() => navigate('/situation')}>시작하기</button>
+            </div>
+        </>
     )
 }
 
