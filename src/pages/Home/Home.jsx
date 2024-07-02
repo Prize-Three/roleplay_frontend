9bd69aa710a6376377595343579269d406f0c3f9
SeoMiYoung <seo20seo00@naver.com>
2024-07-02 21:50:16 +0900

    기획 시안처럼 형태 적용해보기

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,19 +6,16 @@ function Home() {
     let title = 'AI와 함께하는 역할놀이 서비스 테스트 페이지';
 
     return (
-        <>
+        <div className={styles.homeWrap}>
             <div>
                 <button onClick={() => navigate('/voice-manage')}>🔊</button>
                 <button onClick={() => navigate('/total-result')}>📝</button>
-            </div>
-            <div className={styles.homeImg}>
-                <img src="/assets/home/homeImg.png" alt="메인 이미지" width="100%"/>
             </div>
             <div className={styles.homeDiv}>
                 <h4>{title}</h4>
                 <button onClick={() => navigate('/situation')}>시작하기</button>
             </div>
-        </>
+        </div>
     )
 }
 
