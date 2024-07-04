bf69b1c2e8e3c34e403cc8cafeeb8d03022c502d
SeoMiYoung <seo20seo00@naver.com>
2024-07-04 11:55:40 +0900

    세부설정(rightPanel) 토글 기능 추가

--- src/pages/Situation/Situation.jsx
+++ src/pages/Situation/Situation.jsx
@@ -6,9 +6,16 @@ import styles from './Situation.module.scss';
 function Situation() {
     const navigate = useNavigate();
     const [selectedGameType, setSelectedGameType] = useState(null);
+    const [ rightPanelState, setRightPanelState ] = useState(false);
 
     const handleRolePlaySelect = (gameType) => {
-        setSelectedGameType(gameType);
+        if (gameType === selectedGameType) {
+            setSelectedGameType(null); // 이미 선택된 게임 타입을 다시 클릭하면 선택 취소
+            setRightPanelState(false); // rightPanel 숨기기
+        } else {
+            setSelectedGameType(gameType); // 새로운 게임 타입 선택
+            setRightPanelState(true); // rightPanel 보이기
+        }
     };
 
     const handleNextStep = () => {
@@ -46,7 +53,7 @@ function Situation() {
                             <button onClick={() => handleRolePlaySelect('소꿉 놀이')} className={styles.gameTypeButton}>소꿉 놀이</button>
                         </div>
                     </div>
-                    {selectedGameType && (
+                    {rightPanelState && selectedGameType && (
                         <div className={styles.rightPanel}>
                             <h3>{selectedGameType}에 대한 세부 설정</h3>
                             <p>음성 설정</p>
