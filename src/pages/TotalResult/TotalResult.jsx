a8d2384dd19044e18ee314f197b161e5e53b345d
SeoMiYoung <seo20seo00@naver.com>
2024-08-09 16:45:35 +0900

    스크립트 모음 틀 생성

--- src/pages/TotalResult/TotalResult.jsx
+++ src/pages/TotalResult/TotalResult.jsx
@@ -9,10 +9,15 @@ function TotalResult() {
     return (
         <Layout>
             <div className={styles.totalResultContainer}>
-                <div className={styles.whiteBox}>
+                <div className={styles.totalResultHead}>
+                    <div>역할놀이 스크립트 관리</div>
+                    <button onClick={() => navigate('/')}>나가기 ❎</button>
+                </div>
+
+                <div className={styles.totalResultBody}>
                     <h1>스크립트 모음</h1>
                     <p>그동안 진행한 모든 스크립트 확인 가능 페이지</p>
-                    <button onClick={() => navigate('/')}>메인화면으로 돌아가기</button>
+                    
                 </div>
             </div>
         </Layout>
