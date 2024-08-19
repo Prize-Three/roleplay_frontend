bc67566c7af023bf624fae803aa27f1082e88f58
SeoMiYoung <seo20seo00@naver.com>
2024-08-19 19:59:41 +0900

    [Home] 메인페이지 디자인 수정

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,31 @@
-// Home.jsx
-// 메인 화면
-
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../../components/Layout/Layout';
 import { useNavigate } from 'react-router-dom';
 import styles from './Home.module.scss';
 
 function Home() {
     const navigate = useNavigate();
+    const [menuOpen, setMenuOpen] = useState(false);
     let title = 'AI와 함께하는 역할놀이';
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
     return (
         <Layout>
             <div className={styles.homeWrap}>
-                <div className={styles.buttonWrap}>
-                    <button onClick={() => navigate('/voice-manage')}>🧸 캐릭터 소개 (음성 소개)</button>
-                    <button onClick={() => navigate('/total-result')}>📝 결과 모음</button>
+                <div className={styles.hamburgerMenu} onClick={toggleMenu}>
+                    ☰
                 </div>
+                {menuOpen && (
+                    <div className={styles.menu}>
+                        <button onClick={() => navigate('/voice-manage')}>🧸 캐릭터 소개 (음성 소개)</button>
+                        <button onClick={() => navigate('/total-result')}>📝 결과 모음</button>
+                    </div>
+                )}
                 <div className={styles.homeContentWrap}>
                     <div className={styles.homeDiv}>
-                        <h4>{title}</h4>
-                        <img className={styles.agomiImg} src='/assets/common/agomi.png'/>
                         <button className={styles.startBtn} onClick={() => navigate('/situation')}>시작하기</button>
                     </div>
                 </div>
